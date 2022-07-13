b01ec7f9a193c5ec6397ab03dab9246e6e42f757
dpbarlow <dbarlow@trinitytg.com>
2022-07-13 14:10:31 -0700

    End of Section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  // initalizing function which sends get request and returns a response of users
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  // Old Version
+  // getUsers() {
+  //   this.http.get('https://localhost:5001/api/users').subscribe(response => {
+  //     this.users = response;
+  //   }, error => {
+  //     console.log(error);
+  //   })
+  // }
+
+  // Revised Version
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error)
+    })
+  }
+}
